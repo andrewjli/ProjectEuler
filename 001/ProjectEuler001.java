255635f3c19aa33d4ebbff07923906a07f994136
Andrew Li <andrewli92@gmail.com>
2014-07-22 20:58:36 +0800

    Initial commit

---
+++ 001/ProjectEuler001.java
@@ -0,0 +1,19 @@
+/* 
+ * Project Euler Problem 001
+ */
+
+class ProjectEuler001 {
+    public static void main(String[] args) {
+        int counter = 1;
+        int sum = 0;
+
+        while (counter < 1000) {
+        	if ((counter % 3 == 0) || (counter % 5 == 0)) {
+        		sum += counter;
+        	}
+        	counter++;
+        }
+
+        System.out.println(sum);
+    }
+}
